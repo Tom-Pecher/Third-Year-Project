f791e1e2c27eedeeafb20d2df7129cc89053398e
main <tompecher.official@gmail.com>
2025-01-28 11:47:47 +0000

    Implemented saving and loading of models

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
 
 from envs.test_env import TestEnv
-from agents.DQN import DQNAgent
+from agents.DQN_Agent import DQNAgent
 
 if __name__ == "__main__":
     env = TestEnv()
     agent = DQNAgent(env)
     agent.train()
-    
--- src/utils/DQN.py
+++ src/utils/DQN.py
@@ -2,6 +2,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from pathlib import Path
 
 class DQN(nn.Module):
     def __init__(self, input_size:int, output_size:int) -> None:
@@ -16,4 +17,9 @@ class DQN(nn.Module):
 
     def forward(self, x:torch.Tensor) -> torch.Tensor:
         return self.net(x)
-    
+
+    def save(self, filename:str, path="src/saved") -> None:
+        torch.save(self.state_dict(), Path(path) / filename)
+
+    def load(self, filename:str, path="src/saved") -> None:
+        self.load_state_dict(torch.load(Path(path) / filename))
