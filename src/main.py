e88ce987b8dc0d21a9ce4b673d78897bceee0cf6
main <tompecher.official@gmail.com>
2025-03-09 00:09:46 +0000

    Implemented DDQN and DDDQN

--- src/main.py
+++ src/main.py
@@ -2,16 +2,19 @@
 from envs.default.default_env import DefaultTrafficEnv
 from envs.random.random_env import RandomTrafficEnv
 from envs.sanity.sanity_env import SanityTrafficEnv
+
 from agents.DQN.DQN_Agent import DQNAgent
+from agents.DDQN.DDQN_Agent import DDQNAgent
+from agents.DDDQN.DDDQN_Agent import DDDQNAgent
 
 if __name__ == "__main__":
     default_env = DefaultTrafficEnv()
     random_env = RandomTrafficEnv(state_type=1, vehicles_seen=10)
     sanity_env = SanityTrafficEnv()
 
-    agent1 = DQNAgent(random_env, wandb_on=False)
+    agent1 = DDDQNAgent(random_env, wandb_on=False)
     agent1.train(1000)
 
-    agent2 = DQNAgent(sanity_env, wandb_on=False)
+    agent2 = DDDQNAgent(sanity_env, wandb_on=False)
     agent2.load("DQN_1000.pth")
     agent2.run(1, sumo_gui=True)
