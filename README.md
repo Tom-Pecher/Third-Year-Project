2838a56fa8c4cff3567e4b1a8dcbff07a1ace741
main <tompecher.official@gmail.com>
2025-05-02 01:22:08 +0100

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Third-Year-Project
-Smart traffic light system using RL.
+
+## Running the project
+In order to run this project, you will need to have installed SUMO (https://sumo.dlr.de/docs/Installing/index.html), ensuring that it is in your environment variables.
+
+Next, you will need to install the necessary libraries with the help of the "requirements.txt" file.
+
+Finally, you can move into the "src" folder and run "python main.py" which will start training the example agent.
+
+## Project structure
+ - agents: contains a variety of RL and non-RL agents
+ - envs: contains a variety of environments that control states and rewards
+ - networks: contains a variety of road network structures
+ - routes: contains a variety of vehicle flow types
+ - saved: stores models and data during training
+ - sensors: contains lane area detectors for road networks
+ - sumo: folder in which the environment will load all sumo related files (modification is not recommended)
+ - utils: contains a variety of helper classes
+ 
--- src/main.py
+++ src/main.py
@@ -20,29 +20,18 @@ if __name__ == "__main__":
     else:
         sys.exit("Please declare environment variable 'SUMO_HOME'")
 
-    # r = RandomTrafficEnv("X_3", state_type="111", reward_type="111")
-    # r = SanityTrafficEnv("X_3", state_type=2)
+    # Choose an environment:
+    env = RandomTrafficEnv("X_3", state_type="101", reward_type="110")
+    # Select:
+    #  - a road network (e.g. X_3)
+    #  - state type (e.g. 101)
+    #  - reward type (e.g. 110)
 
-    # a = DQNAgent(r, wandb_on=False)
-    # a = DDQNAgent(r, wandb_on=False)
-    # a = DDDQNAgent(r, wandb_on=True)
-    # a = FixedDurationAgent(r, 100, wandb_on=False)
-    # a = RandomAgent(r, 0.5, wandb_on=False)
-
-    # a.train(1, sumo_gui=False)
-    # a.run(1, sumo_gui=True)
-
-    # a.load("DDDQN_20.pth")
-    # a.train(1, sumo_gui=True)
-        
-    env = RandomTrafficEnv("X_3", state_type="111", reward_type="011")
-
+    # Choose an agent:
     agent = DDDQNAgent(env, wandb_on=False)
-    agent.train(25)
+
+    # Train the agent:
+    agent.train(100)
+
+    # Run the agent and view its performance:
     agent.run(1, sumo_gui=True)
-
-    # env2 = RandomTrafficEnv("T_1", state_type="111", reward_type="010")
-
-    # dddqn_agent = DQNAgent(env2, wandb_on=False)
-    # dddqn_agent.load("DQN_25.pth")
-    # dddqn_agent.run(1, sumo_gui=True)
